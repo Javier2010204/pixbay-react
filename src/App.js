96d8d7bb5b910662413def32c418438a56cf0119
JavierSantaCruzXD <2010204javiergomez@gmail.com>
2019-10-03 19:47:01 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Buscador from './components/Buscador';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    termino: ''
+  }
+
+  datosBusqueda = (termino)=>{
+    this.setState({
+      termino
+    })
+  }
+
+  render(){
+    return (
+      <div className="app scontainer">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de imagenes</p>
+          <Buscador
+            datosBusqueda={this.datosBusqueda}
+          />
+        </div>
+        {this.state.termino}
+      </div>
+    );
+  }
 }
 
 export default App;
